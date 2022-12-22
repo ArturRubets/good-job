fae955ba5f7ec37b6d5b1b7c2cdb51f753834ca6
ArturRubets <artur.rubets@gmail.com>
2022-12-22 05:55:43 +0200

    Changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,20 @@
+/* Grab references to the DOM elements */
+
 const searchInputWeather = document.querySelector('.search-input-weather');
 const selectedText = document.querySelector('.selected-text');
 const selectedInput = document.querySelector('.selected-input');
 const searchRemoveText = document.querySelector('.search-remove-text');
 const weatherWidget = document.querySelector('.weather-widget');
-const delay = 2000;
-const weatherLimit = 1;
-const debounce =
-  (callback, delay, timeout = 0) =>
-  (e) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => callback(e), delay);
-  };
+
+/* Define constants */
+
 const apiKey = '77026bf24e9d8b82fd8d44d6069e70cf';
 const weatherDomain = 'https://api.openweathermap.org';
 const iconsDomain = 'https://openweathermap.org';
+const delayDebounce = 2000;
+const weatherGeocodingLimit = 1;
+
+/* Define classes */
 
 class Weather {
   constructor(
@@ -48,59 +49,61 @@ class WeatherCalendar {
   }
 }
 
-searchInputWeather.focus();
+class WeatherAPI {
+  // Query is city.
+  async getGeocoding(query, limit, apiKey) {
+    const url = `${weatherDomain}/geo/1.0/direct?q=
+  ${this.geocodingQueryProcessing(query)}&limit=${limit}&appid=${apiKey}`;
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      console.log(
+        `Network geocoding request for ${query} failed with response ${response.status}: ${response.statusText}`
+      );
+    }
+  }
 
-async function getGeocoding(query, limit, apiKey) {
-  const url = `${weatherDomain}/geo/1.0/direct?q=
-  ${geocodingQueryProcessing(query)}&limit=${limit}&appid=${apiKey}`;
-  console.log(url);
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  } else {
-    console.log(
-      `Network geocoding request for ${query} failed with response ${response.status}: ${response.statusText}`
-    );
+  geocodingQueryProcessing(query) {
+    return query
+      .split(/[\s,]+/)
+      .join(',')
+      .trim();
+  }
+
+  async getWeather(lat, lon, apiKey) {
+    if (!lat || !lon) {
+      return;
+    }
+    const url = `${weatherDomain}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      console.log(
+        `Network weather request for lat: ${lat} and lon: ${lon} failed with response ${response.status}: ${response.statusText}`
+      );
+    }
   }
 }
 
-const geocodingQueryProcessing = (query) =>
-  query
-    .split(/[\s,]+/)
-    .join(',')
-    .trim();
+/* Define functions */
 
-async function getWeather(lat, lon, apiKey) {
-  if (!lat || !lon) {
-    return;
-  }
-  const url = `${weatherDomain}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  console.log(url);
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  } else {
-    console.log(
-      `Network weather request for lat: ${lat} and lon: ${lon} failed with response ${response.status}: ${response.statusText}`
-    );
-  }
-}
+const debounce =
+  (callback, delay, timeout = 0) =>
+  (e) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callback(e), delay);
+  };
 
-searchRemoveText.addEventListener('click', removeSearch);
-
-function removeSearch() {
+const removeSearch = () => {
   searchInputWeather.value = '';
   selectedText.textContent = '';
   selectedInput.style.display = 'none';
   searchInputWeather.focus();
-}
+};
 
-searchInputWeather.addEventListener(
-  'input',
-  debounce(handleWeatherInput, delay)
-);
-
-async function handleWeatherInput(e) {
+const handleWeatherInput = async (e, weatherClient) => {
   const rawQuery = e.target.value;
   if (rawQuery.trim() === '') {
     removeSearch();
@@ -111,31 +114,49 @@ async function handleWeatherInput(e) {
   selectedInput.style.display = 'block';
 
   try {
-    const geocoding = await getGeocoding(rawQuery, weatherLimit, apiKey);
-    const firstCity = geocoding[0];
+    const geocoding = await weatherClient.getGeocoding(
+      rawQuery,
+      weatherGeocodingLimit,
+      apiKey
+    );
 
-    const forecast = await getWeather(firstCity.lat, firstCity.lon, apiKey);
-    const weatherCalendar = handleAPIForecast(forecast);
+    const firstCity = geocoding[0];
+    if (!firstCity) {
+      return;
+    }
+
+    const jsonWeather = await weatherClient.getWeather(
+      firstCity.lat,
+      firstCity.lon,
+      apiKey
+    );
+    const weatherCalendar = handleJsonWeather(jsonWeather);
 
     displayWeather(
       weatherCalendar.currentWeather,
       weatherCalendar.forecastWeatherArray
     );
+
+    setBackground(jsonWeather.city.name + ' ' + jsonWeather.city.country);
   } catch (error) {
     displayFailedWeatherSearch(error);
   }
-}
+};
 
-function handleAPIForecast(forecast) {
+const handleJsonWeather = (jsonWeather) => {
+  if (!jsonWeather) {
+    return;
+  }
+
   const currentDate = new Date().toDateString();
 
   // forecast.list contains the weather forecast for every three hours.
   // tempArray contains only one forecast for each day at 12:00,
   // but for the current day, the forecast will be the first in the list.
-  const tempArray = forecast.list
+  const tempArray = jsonWeather.list
     .filter(
       (f) =>
-        forecast.list[0].dt_txt === f.dt_txt ||
+        jsonWeather.list[0].dt_txt === f.dt_txt ||
         (f.dt_txt.includes('12:00') &&
           new Date(f.dt_txt).toDateString() !== currentDate)
     )
@@ -150,14 +171,14 @@ function handleAPIForecast(forecast) {
           f.weather[0].description,
           f.weather[0].icon,
           f.dt_txt,
-          forecast.city.country,
-          forecast.city.name
+          jsonWeather.city.country,
+          jsonWeather.city.name
         )
     );
   return new WeatherCalendar(tempArray[0], tempArray.slice(1));
-}
+};
 
-function displayWeather(currentWeather, forecastWeatherArray) {
+const displayWeather = (currentWeather, forecastWeatherArray) => {
   try {
     const currentWeatherHTML = (() => {
       const temperature = Math.round(currentWeather.temperature);
@@ -228,25 +249,62 @@ function displayWeather(currentWeather, forecastWeatherArray) {
   } catch (error) {
     displayFailedWeatherSearch(error);
   }
-}
+};
 
-function displayFailedWeatherSearch(error) {
+const displayFailedWeatherSearch = (error) => {
   weatherWidget.style.display = 'none';
   console.log(error);
-}
+};
 
-// Getting the weather according to the user's location
-function initialWeatherSearch() {
+// Getting the weather according to the user's location.
+const initialWeatherSearch = (weatherClient) => {
   navigator.geolocation.getCurrentPosition(async (position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const forecast = await getWeather(lat, lon, apiKey);
-    const weatherCalendar = handleAPIForecast(forecast);
+    const { latitude, longitude } = position.coords;
+    const jsonWeather = await weatherClient.getWeather(
+      latitude,
+      longitude,
+      apiKey
+    );
+    const weatherCalendar = handleJsonWeather(jsonWeather);
+    if (!weatherCalendar) {
+      return;
+    }
     displayWeather(
       weatherCalendar.currentWeather,
       weatherCalendar.forecastWeatherArray
     );
-  });
-}
 
-initialWeatherSearch();
+    setBackground(jsonWeather.city.name + ' ' + jsonWeather.city.country);
+  });
+};
+
+const getRandomImageByContext = (contextText) => {
+  return `https://source.unsplash.com/1600x900/?${contextText}`;
+};
+
+const setBackground = (searchImage) => {
+  document.body.style.backgroundImage = `url("${getRandomImageByContext(
+    searchImage
+  )}")`;
+};
+
+/* Program implementation */
+
+searchInputWeather.focus();
+
+searchRemoveText.addEventListener('click', removeSearch);
+
+const weatherClient = new WeatherAPI();
+
+searchInputWeather.addEventListener(
+  'input',
+  debounce((e) => handleWeatherInput(e, weatherClient), delayDebounce)
+);
+
+searchInputWeather.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    handleWeatherInput(e, weatherClient);
+  }
+});
+
+initialWeatherSearch(weatherClient);
